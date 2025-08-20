c132305d8305c4726db66a0f2e825664c73d75c9
Glover69 <iamglover02@gmail.com>
2025-08-20 00:56:21 +0000

    added endpoint for deleting schedules

--- src/schedulr/routes/user.routes.ts
+++ src/schedulr/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createSchedule, getUserSchedules } from "../controllers/user.controller";
+import { createSchedule, deleteSchedule, getUserSchedules } from "../controllers/user.controller";
 const router = express.Router()
 
 
 router.get("/get-data", getUserSchedules)
 router.post("/save-schedule", createSchedule)
+router.delete("/delete-one", deleteSchedule)
 
 
 export default router
